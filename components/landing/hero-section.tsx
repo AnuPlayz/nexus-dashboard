57d2f526ddb9eeed78df78e27d191e80bf002c58
Aniruddh <adubge@gmail.com>
2024-04-20 08:21:20 +0530

    Update link in HeroSection component to point to /user instead of http://localhost:3000/

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -34,7 +34,7 @@ export default function HeroSection() {
         data visualization.
       </p>
       <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-black opacity-0 ease-in-out [--animation-delay:600ms]">
-        <Link href="http://localhost:3000/">
+        <Link href="/user">
           <span>Get Started for free </span>
         </Link>
         <ArrowRightIcon className="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
