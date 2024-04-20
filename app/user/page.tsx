83cbf142aa439d977a413f2923698ee8fb82b65b
Aniruddh <adubge@gmail.com>
2024-04-20 08:17:41 +0530

    Update iframe styling in user page component

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className='w-full h-vh block'>
+        <div className='w-full h-screen block mt-[64px]'>
             <iframe
-                className='w-full h-vh block'
+                className='w-full h-full'
                 src="https://9b9b19992dd6.quillforms.app/forms/simple-donation-form/"
                 allowFullScreen
             />
