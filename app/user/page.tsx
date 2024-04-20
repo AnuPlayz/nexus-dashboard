eb58e281297f78c8f9e5fe887f04fac860ee86bf
Aniruddh <adubge@gmail.com>
2024-04-20 08:36:21 +0530

    Update link in HeroSection component to point to /user instead of http://localhost:3000/

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
     }
     else {
         return (
-            <div className="flex flex-row items-center justify-center w-full h-screen mt-[64px]">
+            <div className="flex flex-col items-center justify-center w-full h-screen mt-[64px]">
                 <h1 className="text-2xl">Please sign in to access this page</h1>
                 <UserAuthForm />
             </div>
