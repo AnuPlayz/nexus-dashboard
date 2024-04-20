69a740ce865345788800a6e1470e63228ea37f1f
Aniruddh <adubge@gmail.com>
2024-04-20 08:08:19 +0530

    Update iframe styling in user page component

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,6 +4,7 @@ const Page = () => {
     return (
         <div className='w-full h-dvh'>
             <iframe
+                className='w-full h-dvh'
                 src="https://9b9b19992dd6.quillforms.app/forms/simple-donation-form/"
                 allowFullScreen
             />
